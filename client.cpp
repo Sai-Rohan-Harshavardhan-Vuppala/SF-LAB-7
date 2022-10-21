6cb1ef69395259070321cba27a66e2a1c6b83ec3
Sai Rohan Harshavardhan Vuppala <shv12@iitbbs.ac.in>
2022-10-21 18:56:22 +0530

    first commit

---
+++ client.cpp
@@ -0,0 +1,328 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <bits/stdc++.h>
+using namespace std;
+#define PORT "3490"		 // the port client will be connecting to
+#define MAXDATASIZE 1000 // max number of bytes we can get at once
+// get sockaddr, IPv4 or IPv6:
+typedef struct data1
+{
+	int sockfd;
+} data1;
+int status, check;
+string stat = "FREE";
+long long int g = -1, a, B, x;
+string key, client_id, from = "server";
+string extractFromLastLine(char buf[])
+{
+	string temp;
+	int i;
+	for(i = strlen(buf) - 1; buf[i] != '\n'; i--){
+		temp.push_back(buf[i]);
+	}
+	buf[i] = '\0';
+	reverse(temp.begin(), temp.end());
+	cout << temp;
+	return temp;
+}
+vector<int> RC4(vector<int> &ip, vector<int>&K, int &K_len)
+{
+    int S[256], T[256];
+    // Initialization
+    int i;
+    for(i = 0; i < 256; i++){
+        S[i] = i;
+        T[i] = K[i%K_len];
+    }
+    int j = 0;
+    for(i = 0;i < 256; i++){
+        j = (j + S[i] + T[i])%256;
+        int temp = S[i];
+        S[i]  = S[j];
+        S[j] = temp;
+    }
+
+    // Stream generation
+    i = 0, j = 0;
+    vector<int>K1;
+    for(int t = 0; t < ip.size(); t++){
+        i = (i + 1)%256;
+        j = (j + S[i])%256;
+        int temp = S[i];
+        S[i] = S[j];
+        S[j] = temp;
+        temp = (S[i] + S[j])%256;
+        K1.push_back(S[temp]);
+    }
+    cout << "\n";
+    // Encrypting using XOR
+    vector<int>op;
+    for(int i=0;i<ip.size();i++){
+        int p = K1[i]^ip[i];
+        op.push_back(p);
+    }
+    return op;
+}
+void RC4_Cipher(char buf[])
+{
+	vector<int>ip, K;
+	int K_len, i;
+	for(int i = 0; buf[i] != '\0'; i++){
+		ip.push_back(buf[i]);
+	}
+	for(int i = 0; i < key.size(); i++){
+		K.push_back(key[i]);
+	}
+	K_len = K.size();
+	vector<int>op = RC4(ip, K, K_len);
+	for(i = 0; buf[i] != '\0'; i++){
+		buf[i] = op[i];
+	}
+	buf[i] = '\0';
+}
+bool substring(char buf[], string s)
+{
+	for(int i = 0; i < s.size(); i++){
+		if(buf[i] == '\0' || buf[i] != s[i]) return false;
+	}
+	return true;
+}
+long long int power(long long int B, long long int x, long long int a)
+{
+	long long int ans = 1;
+	while(x--){
+		ans = (ans*B)%a;
+	}
+	return ans;
+}
+void *solve(void *p)
+{
+	char buf[MAXDATASIZE];
+	int numbytes;
+	data1 *ptr = (data1 *)p;
+	string s = " good bye", temp;
+	int i, n;
+	numbytes = recv(ptr->sockfd, buf, MAXDATASIZE - 1, 0);
+	buf[numbytes] = '\0';
+	for(i = 86; buf[i]!=' '; i++){
+		temp.push_back(buf[i]);
+	}
+	cout << temp;
+	g = stoll(temp);
+	temp = "";
+	for(i++; buf[i]!='\0'; i++){
+		temp.push_back(buf[i]);
+	}
+	a = stoll(temp);
+	temp = "";
+	buf[86] = '\0';
+	printf("\n%s\n", buf);
+	printf("\nClient:");
+	while (1)
+	{
+		numbytes = recv(ptr->sockfd, buf, MAXDATASIZE - 1, 0);
+		if (numbytes == -1)
+		{
+			perror("recv");
+			break;
+		}
+		if (numbytes == 0)
+		{
+			printf("Socket disconnected*\n");
+			break;
+		}
+		buf[numbytes] = '\0';
+		if(stat == "BUSY"){
+			RC4_Cipher(buf);
+			printf("%s\n", buf);
+			if(buf[numbytes - 1] == 'S') from = "server";
+			else from = "client";
+		}
+		numbytes--;
+		buf[numbytes] = '\0';
+		if(from == "server")
+		{
+			if (check == 0 && substring(buf, "Server:\nYou can now chat with the client"))
+			{
+				if(stat == "PENDING-R")
+				{
+					B = stoll(extractFromLastLine(buf));
+					key = to_string(power(B, x, a));
+				}
+				stat = "BUSY";
+				check = 1;
+			}
+			else if (check == 0 && substring(buf, "Server:\nWould")){
+				printf("%s\n", buf);
+				for(int i = 44; buf[i] != '?'; i++){
+					client_id.push_back(buf[i]);
+				}
+				B = stoll(extractFromLastLine(buf));
+				stat = "PENDING-A";
+			}
+			else if (check == 1 && buf[0] == 'S')
+			{
+				check = 0;
+				stat = "FREE";
+				client_id = "";
+			}	
+		}
+		else if (check == 1)
+		{
+			for (i = 0; buf[i] != '\0'; i++)
+				if (buf[i] == ':')
+					break;
+			i++;
+			if (numbytes - i == s.size())
+			{
+				for (; s[i] != '\0'; i++)
+					if (s[i] != buf[i])
+						break;
+				if (s[i] == '\0')
+					check = 0, stat = "FREE", client_id = "";
+			}
+		}
+		if(from == "client") cout << "Client-" << client_id << ":\n";
+		printf("\n%s\n", buf);
+		cout << "\nClient:\n";
+	}
+	status = 1;
+	pthread_exit(NULL);
+}
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET)
+	{
+		return &(((struct sockaddr_in *)sa)->sin_addr);
+	}
+	return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
+void choosePrivateKey(char buf[])
+{
+	x = rand();
+	string A = to_string(power(g, x, a));
+	int n = strlen(buf), i;
+	buf[n] = '\n';
+	n++;
+	for(i = 0; i < A.size(); i++){
+		buf[i + n] = A[i];
+	}
+	buf[i + n] = '\0';
+	printf("D-220-%s\n", buf);
+}
+int main(int argc, char *argv[])
+{
+	int sockfd, numbytes;
+	char buf[MAXDATASIZE];
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	char s[INET6_ADDRSTRLEN];
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: Client hostname\n");
+		exit(1);
+	}
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0)
+	{
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+	// loop through all the results and connect to the first we can
+	for (p = servinfo; p != NULL; p = p->ai_next)
+	{
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+		{
+			perror("Client: socket");
+			continue;
+		}
+		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+		{
+			close(sockfd);
+			perror("Client: connect");
+			continue;
+		}
+		break;
+	}
+	if (p == NULL)
+	{
+		fprintf(stderr, "client: failed to connect\n");
+		return 2;
+	}
+	status = 0;
+	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
+	printf("Client: connecting to %s\n", s);
+	freeaddrinfo(servinfo); // all done with this structure
+	data1 pthread_data;
+	pthread_data.sockfd = sockfd;
+	pthread_t tid;
+	pthread_create(&tid, NULL, solve, &pthread_data);
+	while (1)
+	{
+		if (status == 1)
+			break;
+		scanf(" %[^\n]s", buf);
+		// printf("\n");
+		// cout << "--\n";
+		printf("------------------\n");
+		//cout << check << "\n";
+		if (check==0 && stat == "FREE" && substring(buf, "connect"))
+		{
+			int i;
+			for(i = 7; buf[i] != ' '; i++);
+			string temp;
+			for(; '0' <= buf[i] && buf[i] <= '9'; i++) temp.push_back(buf[i]);
+			client_id = temp;
+			stat = "PENDING-R";
+			choosePrivateKey(buf);
+		}
+		else if (check == 0 && stat == "PENDING-A" && !strcmp(buf, "yes"))
+		{
+			choosePrivateKey(buf);
+			key = to_string(power(B, x, a));
+		}
+		else if (check==1 && stat == "BUSY" && !strcmp(buf, "good bye"))
+		{
+			cout << "\nServer:\n***Chat has ended***\n\nClient:\n";
+			check = 0;
+			stat = "FREE";
+		}
+		else if (check == 1 && stat == "BUSY")
+		{
+			int n = strlen(buf);
+			buf[n] = 'C';
+			buf[n + 1] = '\0';
+			RC4_Cipher(buf);
+			for(int i = 0; i < strlen(buf); i++) cout << buf[i] << " ";
+			cout << "\n";
+		}
+		else if ((!strcmp(buf, "close") && !check) || status)
+		{
+			//cout << "please leave ra\n";
+			if (send(sockfd, buf, strlen(buf), 0) == -1)
+			{
+				perror("send");
+				exit(1);
+			}
+			break;
+		}
+		if (send(sockfd, buf, strlen(buf), 0) == -1)
+		{
+			perror("send");
+			exit(1);
+		}
+	}
+	close(sockfd);
+	return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,442 @@
+#include <bits/stdc++.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#define MAXDATASIZE 1000 // max number of bytes we can get at once
+#define MAXDATASIZE 1000 // max number of clients that can be conncected to client
+#define PORT "3490"		 // the port users will be connecting to
+#define BACKLOG 10		 // how many pending connections queue will hold
+using namespace std;
+typedef struct data1{
+	int id;
+	int destid;
+	int sourcefd;
+	string status;
+	int destfd;
+} data1;
+vector<data1> v;
+int c = 1;
+long long int g, a;
+string extractFromLastLine(char buf[])
+{
+	string temp;
+	int i;
+	for(i = strlen(buf) - 1; buf[i] != '\n'; i--){
+		temp.push_back(buf[i]);
+	}
+	buf[i] = '\0';
+	reverse(temp.begin(), temp.end());
+	cout << temp;
+	return temp;
+}
+void copy(string &s, char buf[], int choice)
+{
+	int i;
+	if (choice == 0)
+	{
+		for (i = 0; buf[i] != '\0'; i++)
+			s.push_back(buf[i]);
+	}
+	else
+	{
+		for (i = 0; s[i] != '\0'; i++)
+			buf[i] = s[i];
+		buf[i] = '\0';
+	}
+}
+bool substring(char buf[], string s)
+{
+	for(int i = 0; i < s.size(); i++){
+		if(buf[i] == '\0' || buf[i] != s[i]) return false;
+	}
+	return true;
+}
+void *solve(void *p)
+{
+	data1 *ptr = (data1 *)p;
+	int idx = ptr->id;
+	char buf[MAXDATASIZE];
+	string intro = "Server:\nHello Client\nEnter <list> to see the list of other clients connected to server" + to_string(g)+ " "+to_string(a);
+	copy(intro, buf, 1);
+	// strcpy(buf, "Server:\nHello Client\nEnter <list> to see the list of other clients connected to server");
+	if (send(ptr->sourcefd, buf, strlen(buf), 0) == -1)
+		perror("send");
+	printf("%s\n", buf);
+	int numbytes, c;
+	string str;
+
+	while (1)
+	{
+		numbytes = recv(v[idx].sourcefd, buf, MAXDATASIZE - 1, 0);
+		if (numbytes == -1)
+		{
+			if (v[idx].destid != -1)
+			{
+				v[v[idx].destid].destid=-1;
+				v[v[idx].destid].status = "FREE";
+			}
+			v[idx].destid = -1;
+			v[idx].id = -1;
+			perror("recv");
+			break;
+		}
+		if (numbytes == 0)
+		{
+			if (v[idx].destid != -1 && v[v[idx].destid].id!=-1)
+			{
+				if(v[v[idx].destid].status == "PENDING-A")
+				strcpy(buf, "Server:\n***Requesting Client was disconnected unexpectedly***S");
+				else if(v[v[idx].destid].status == "BUSY")
+				strcpy(buf, "Server:\n***Chat has ended***S");
+				else if(v[v[idx].destid].status == "PENDING-R")
+				strcpy(buf, "Server:\n***Requested Client was disconnected unexpectedly***S");
+				v[v[idx].destid].status = "FREE";
+				if (send(v[idx].destfd, buf, strlen(buf), 0) == -1)
+				{
+					v[idx].destid = -1;
+					v[idx].id = -1;
+					perror("send");
+					pthread_exit(NULL);
+				}
+			}
+			v[idx].destid = -1;
+			v[idx].id = -1;
+			printf("Socket disconnected\n");
+			break;
+		}
+		buf[numbytes] = '\0';
+		cout << pthread_self() << "-" << idx << " " << buf << " " << numbytes << "\n";
+		if (v[idx].status == "FREE")
+		{
+			if (!strcmp(buf, "close"))
+			{
+				if (send(v[idx].sourcefd, buf, strlen(buf), 0) == -1)
+				{
+					close(v[idx].sourcefd);
+					v[idx].id = -1;
+					perror("send");
+					pthread_exit(NULL);
+				}
+				v[idx].destid = -1;
+				v[idx].id = -1;
+				close(v[idx].sourcefd);
+				pthread_exit(NULL);
+			}
+			else if (!strcmp(buf, "list"))
+			{
+				int i, n = v.size();
+				string s = "Server:\n";
+				for (i = 0; i < n; i++)
+				{
+					if (v[i].id != -1 && v[idx].id != v[i].id)
+						s = s + "Client-" + to_string(v[i].id) + " Status:" + v[i].status + "\n";
+					else if (v[idx].id == v[i].id)
+						s = s + "Client-" + to_string(v[i].id) + " Status:" + v[i].status + " (YOU)\n";
+				}
+				s = s + "Enter <connect <client-id>> to connect to the corresponding clientS";
+				copy(s, buf, 1);
+			}
+			else
+			{
+				string s;
+				int i, k, chk=0,t;
+				copy(s,buf,0);
+				if (s.substr(0, 7) != "connect")
+					chk=1;
+				else
+				{
+					v[idx].status = "PENDING-R";
+					for (i = 7; s[i] != '\0'; i++)
+						if (s[i] != ' ')
+							break;
+					if(s[i]!='\0')
+					k = stoi(s.substr(i));
+					else
+					chk=1;
+					for(;s[i]!='\n';i++)
+						if('0'> s[i] || s[i] >'9')
+						{
+							chk=1;
+							break;
+						}
+					if(chk==1)
+					{
+						v[idx].status = "FREE";
+					}
+					else if(k>=v.size() || v[k].id==-1)
+					{
+						v[idx].status = "FREE";	
+						strcpy(buf, "Server:\nInvalid Client-IdS");
+					}
+					else if (k==idx || v[k].status == "FREE")
+					{
+						v[k].status = "PENDING-A";
+						v[k].destid = idx;
+						v[idx].destid = k;
+						v[k].destfd = v[idx].sourcefd;
+						v[idx].destfd = v[k].sourcefd;
+						
+						if (k != idx)
+						{
+							string temp;
+							while(s.size() >= 0 && s.back() != '\n'){
+								temp.push_back(s.back());
+								s.pop_back();
+							}
+							reverse(temp.begin(), temp.end());
+							s="Server:\nWould you like to chat with Client-"+to_string(idx)+"?\nEnter <yes> to accept and <no> to deny the request\n" + temp + "S";
+							copy(s,buf,1);
+							if (send(v[idx].destfd, buf, strlen(buf), 0) == -1)
+							{
+								v[idx].destid = -1;
+								v[idx].id = -1;
+								perror("send");
+								pthread_exit(NULL);
+							}
+							continue;
+						}
+						v[idx].status = "BUSY";
+						strcpy(buf,"Server:\nYou can now chat with the clientS");
+					}
+					else
+					{
+						v[idx].status = "FREE";	
+						v[idx].destid=-1;
+						strcpy(buf, "Server:\nClient is busy at the momentS");
+					}
+				}
+				if(chk==1)
+				{
+					cout << "I am Free" << "\n";
+					strcpy(buf, "Server:\nInvalid CommandS");
+				}
+			}
+			if (send(v[idx].sourcefd, buf, strlen(buf), 0) == -1)
+			{
+				if (v[idx].destid != -1)
+				{
+					v[v[idx].destid].status = "FREE";
+					v[v[idx].destid].destid=-1;
+					v[idx].destid = -1;
+				}
+				v[idx].id = -1;
+				perror("send");
+				pthread_exit(NULL);
+			}
+		}
+		else if(v[idx].status=="PENDING-A")
+		{
+			if(substring(buf,"yes"))
+			{
+				string temp = extractFromLastLine(buf);
+				v[idx].status="BUSY";
+				v[v[idx].destid].status="BUSY";
+				temp = "Server:\nYou can now chat with the client\n" + temp+"S";
+				copy(temp, buf, 1);
+				if (send(v[idx].sourcefd, buf, strlen(buf), 0) == -1)
+				{
+					if (v[idx].destid != -1)
+					{
+						v[v[idx].destid].status = "FREE";
+						v[v[idx].destid].destid=-1;
+						v[idx].destid = -1;
+					}
+					v[idx].id = -1;
+					perror("send");
+					pthread_exit(NULL);
+				}
+				strcpy(buf,"Server:\nClient has accepted your connection request\nYou can now chat with the clientS");
+			}
+			else
+			{
+				v[idx].status="FREE";
+				v[v[idx].destid].status="FREE";
+				v[v[idx].destid].destid=-1;
+				v[idx].destid = -1;
+				strcpy(buf,"Server:\nClient has denied your connection requestS");
+			}
+			if (send(v[idx].destfd, buf, strlen(buf), 0) == -1)
+			{
+				if (v[idx].destid != -1)
+				{
+					v[v[idx].destid].status = "FREE";
+					v[v[idx].destid].destid=-1;
+					v[idx].destid = -1;
+				}
+				v[idx].id = -1;
+				perror("send");
+				pthread_exit(NULL);
+			}
+		}
+		else if(v[idx].status=="PENDING-R")
+		{
+			strcpy(buf,"Server:\nConnection request pending\nPlease wait!S");
+			if (send(v[idx].sourcefd, buf, strlen(buf), 0) == -1)
+			{
+				if (v[idx].destid != -1)
+				{
+					v[idx].status="FREE";
+					v[v[idx].destid].status = "FREE";
+					v[v[idx].destid].destid=-1;
+					v[idx].destid = -1;
+				}
+				v[idx].id = -1;
+				perror("send");
+				pthread_exit(NULL);
+			}
+		}
+		else
+		{
+			cout << 101 << "\n";
+			// str = "Client-" + to_string(idx) + ":\n";
+			if (!strcmp(buf, "good bye"))
+			{
+				numbytes = recv(v[idx].sourcefd, buf, MAXDATASIZE - 1, 0);
+				v[idx].status = "FREE";
+				v[v[idx].destid].status = "FREE";
+				cout << "FREE\n";
+				if(v[idx].destid==idx)
+				{
+					v[idx].destid = -1;
+					continue;
+				}
+			}
+			// cout << str << "\n";
+			// copy(str, buf, 0);
+			// copy(str, buf, 1);
+			if (send(v[idx].destfd, buf, strlen(buf), 0) == -1)
+			{
+				if (v[idx].destid != -1)
+				{
+					v[v[idx].destid].status = "FREE";
+					v[v[idx].destid].destid=-1;
+					v[idx].destid = -1;
+				}
+				v[idx].id = -1;
+				perror("send");
+				pthread_exit(NULL);
+			}
+			strcpy(buf, "\n\nServer:\n***Chat has ended***S");
+			if(v[v[idx].destid].status == "BUSY")
+			{
+			}
+			else if (send(v[idx].destfd, buf, strlen(buf), 0) == -1)
+			{
+				if (v[idx].destid != -1)
+				{
+					v[v[idx].destid].status = "FREE";
+					v[idx].destid = -1;
+				}
+				v[idx].id = -1;
+				perror("send");
+				pthread_exit(NULL);
+			}
+		}
+	}
+	v[idx].destid = -1;
+	v[idx].id = -1;
+	close(v[idx].sourcefd);
+	return NULL;
+}
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET)
+	{
+		return &(((struct sockaddr_in *)sa)->sin_addr);
+	}
+	return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
+int main(void)
+{
+	// r
+	cout << "Enter the value of g: ";
+	cin >> g;
+	cout << "Enter the value of a: ";
+	cin >> a;
+	// r
+	char buf[MAXDATASIZE];
+	int sockfd, new_fd; // listen on sock_fd, new connection on new_fd
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; // connector's address information
+	socklen_t sin_size;
+	// struct sigaction sa;
+	int yes = 1;
+	char s[INET6_ADDRSTRLEN];
+	int rv;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
+	{
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+	// loop through all the results and bind to the first we can
+	for (p = servinfo; p != NULL; p = p->ai_next)
+	{
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+		{
+			perror("Server: socket");
+			continue;
+		}
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+		{
+			perror("setsockopt");
+			exit(1);
+		}
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+		{
+			close(sockfd);
+			perror("Server: bind");
+			continue;
+		}
+		break;
+	}
+	freeaddrinfo(servinfo); // all done with this structure
+	if (p == NULL)
+	{
+		fprintf(stderr, "Server: failed to bind\n");
+		exit(1);
+	}
+	if (listen(sockfd, BACKLOG) == -1)
+	{
+		perror("listen");
+		exit(1);
+	}
+	printf("Server: Waiting for connections...\n");
+	while (1)
+	{ // main accept() loop
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1)
+		{
+			perror("accept");
+			continue;
+		}
+		inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+		printf("Server: got connection from %s\n", s);
+		pthread_t tid;
+		data1 pthread_data;
+		pthread_data.id = v.size();
+		pthread_data.destid = -1;
+		pthread_data.sourcefd = new_fd;
+		pthread_data.status = "FREE";
+		pthread_data.destfd = -1;
+		v.push_back(pthread_data);
+		pthread_create(&tid, NULL, solve, &pthread_data);
+	}
+	return 0;
+}
